5961d6938a443126202f25d8974c55c4aa552a97
meteoben <meteoben@gmail.com>
2024-02-16 14:10:57 +0100

    return true

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 // // server/index.js
 
 // // const express = require("express");
@@ -23,18 +18,12 @@
 //       console.log(`server listening on port ${PORT}`)
 // })
 
-
-
-
-
-
 // const path = require('path');
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const nodemailer = require('nodemailer');
-const bodyParser = require ('body-parser')
-
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 5000;
 
@@ -43,104 +32,69 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
-app.get("/api/contact", (req,res) =>{
-    res.json ({ message: "Hello from server!"})
+app.get("/api/contact", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
 
 const contactEmail = nodemailer.createTransport({
-    service: 'gmail',
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.EMAIL_ADRESS,
-        pass: process.env.EMAIL_PASS
-    }
-
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_ADRESS,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
-contactEmail.verify((error) =>{
-    if(error) {
-        console.log(error)
-    }else {
-        console.log("ready to send")
-    }
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("ready to send");
+  }
 });
 
-app.post("/api/contact", bodyParser.urlencoded({ extended: false}), (req, res) => {
+app.post(
+  "/api/contact",
+  bodyParser.urlencoded({ extended: false }),
+  (req, res) => {
     const name = req.body.firstName + req.body.lastName;
     const email = req.body.email;
     const message = req.body.message;
     const phone = req.body.phone;
     const mail = {
-        from: name,
-        to: process.env.EMAIL_CONTACT,
-        subject : "Contact Form Submission - Portfolio",
-        html: `<p>Name: ${name}</p>
+      from: name,
+      to: process.env.EMAIL_CONTACT,
+      subject: "Contact Form Submission - Portfolio",
+      html: `<p>Name: ${name}</p>
                <p>Email: ${email}</p>
                <p>Phone: ${phone}</p>
                <p>Message: ${message}</p>
-        `
-    }
+        `,
+    };
 
-    contactEmail.sendMail(mail, (error) =>{
-        if (error) {
-            res.json(error);
-        } else {
-            res.json({ code: 200, message :" message sent "})
-        }
+    contactEmail.sendMail(mail, (error) => {
+      if (error) {
+        res.json(error);
+      } else {
+        res.json({ code: 200, message: " message sent " });
+      }
     });
 
-    
-});
-
-
-
+    // return true;
+  }
+);
 
 // });
-
 
 // app.get('*', (req, res) =>{
 //     res.sendFile(path.resolve(__dirname, '../buid', 'index.html'))
 // })
 
-
-app.listen(PORT, () =>{
-    console.log(`Server is listening on port: ${PORT}`);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
 
 // const path = require('path');
 // const express = require('express');
@@ -149,14 +103,12 @@ app.listen(PORT, () =>{
 // const nodemailer = require('nodemailer');
 // const bodyParser = require ('body-parser')
 
-
 // const PORT = process.env.PORT || 3001;
 
 // const app = express();
 // app.use(express.static(path.resolve(__dirname, '../build')))
 // app.use(cors());
 // app.use(bodyParser.json());
-
 
 // app.get("/api", (req,res) =>{
 //     res.json ({ message: "Hello from server!"})
@@ -204,11 +156,9 @@ app.listen(PORT, () =>{
 
 // });
 
-
 // app.get('*', (req, res) =>{
 //     res.sendFile(path.resolve(__dirname, '../buid', 'index.html'))
 // })
-
 
 // app.listen(PORT, () =>{
 //     console.log(`Server is listening on port: ${PORT}`);
